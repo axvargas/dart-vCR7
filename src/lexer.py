a7f25b1b53b950f9281e6a8b8e003dc2eb8916ef
enmanuel-mag <enmanuelmag@cardor.dev>
2021-06-19 01:48:07 -0500

    Add more token and regexs

--- src/lexer.py
+++ src/lexer.py
@@ -1,14 +1,18 @@
 from ply.lex import TOKEN, Token
 import ply.lex as lex
-import codecs
-import re
-import sys
-import os
+
 
 tokens = [
+    'AND',
+    'ARROW', # @ replace => in arrow function
     'ASSIGN',
+    'ASIGSUM',
+    'ASIGTIMES',
+    'ASIGDIV',
+    'MOD',
     'COMMA',
     'DIVISION',
+    'DDOTS',
     'EQ',
     'EQ_V',
     'GT',
@@ -18,17 +22,24 @@ tokens = [
     'FLOAT',
     'LBRACE',
     'RBRACE',
+    'LBRACKET',
+    'RBRACKET',
     'LT',
     'LTE',
     'NEGATION',
     'NE',
     'MINUS',
+    'MINUSMINUS',
+    'OR',
+    'ASIGMINUS',
     'TIMES',
     'LPAREN',
     'PLUS',
+    'PLUSPLUS',
     'RPAREN',
     'SEMICOLON',
-    'ARROW' # @ replace => in arrow function
+    'STRING',
+    'VIR'
 ]
 
 reserved_words = {
@@ -37,8 +48,10 @@ reserved_words = {
     'assert': 'ASSERT',
     'async': 'ASYNC',
     'await': 'AWAIT',
+    'bool': 'BOOLEAN',
     'break': 'BREAK',
     'dynamic': 'DYNAMIC',
+    'double': 'DOUBLE_TYPE',
     'else': 'ELSE',
     'enum': 'ENUM',
     'export': 'EXPORT',
@@ -56,6 +69,7 @@ reserved_words = {
     'for': 'FOR',
     'function': 'FUNCTION',
     'if': 'IF',
+    'int': 'INT_TYPE',
     'return': 'RETURN',
     'var': 'VAR',
     'void': 'VOID',
@@ -64,74 +78,144 @@ reserved_words = {
     'finally': 'FINALLY',
     'true': 'TRUE',
     'typedef': 'TYPEDEF',
-    'siu': 'PRINT'
+    'siu': 'PRINT',
+    'String': 'STRING_TYPE',
+    'List': 'LIST'
 }
 
 tokens = tokens + list(reserved_words.values())
 
-t_ignore = ' \t'
+t_ignore = ' \t\x0c'
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_EQ_V = r'='
-t_COMMA = r';'
+t_COMMA = r','
 t_DIVISION = r'/'
 t_EQ = r'=='
 t_GT = r'>'
 t_GTE = r'>='
 t_LBRACE = r'{'
 t_RBRACE = r'}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 t_LT = r'<'
 t_LTE = r'<='
 t_NEGATION = r'!'
 t_NE = r'!='
 t_SEMICOLON = r';'
 t_ARROW = r'@'
+t_DDOTS = r':'
+t_VIR = r'~'
+t_ASIGSUM = r'\+='
+t_ASIGMINUS = r'-='
+t_ASIGTIMES = r'\*='
+t_ASIGDIV = r'/='
+t_MOD = r'%'
+t_PLUSPLUS = r'\+\+'
+t_MINUSMINUS = r'--'
+t_AND = r'&&'
+t_OR = r'\|\|'
 
-re_SIG = t_MINUS + r'?'
-re_INT = re_SIG + r'([0-9]|[1-9][0-9]+)'
-re_FLOAT = re_SIG + re_INT + r'.' + r'[0-0]+'
+#re_SIG = t_MINUS + r'?'
+t_INT = r'\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
+t_FLOAT = r'((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
+t_STRING = r'(\"|\')([^\\\n]|(\\.))*?(\"|\')'
 
-def start_end(regex):
-    #return r'^' + regex + r'$'
-    return regex
-
-@TOKEN(start_end(r'[a-zA-z_][a-zA-z0-9_]*'))
 def t_IDENT(t):
+    r'[A-Za-z_][\w_]*'
     t.type = reserved_words.get(t.value, 'IDENT')
     return t
 
-@TOKEN(re_INT)
-def t_INT(t):
+""" @TOKEN(re_INT)
+def _INT(t):
     t.value = int(t.value)
     return t
 
 @TOKEN(re_FLOAT)
-def t_FLOAT(t):
+def _FLOAT(t):
     t.value = float(t.value)
-    return t
+    return t """
 
-def t_newline(t):
+def t_NEWLINE(t):
     r'\n+'
-    t.lexer.lineno += len(t.value)
+    t.lexer.lineno += t.value.count("\n")
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Illegal character '%s'" % t.value)
     t.lexer.skip(1)
 
-lexer = lex.lex()
+def t_preprocessor(t):
+    r'\#(.)*?\n'
+    t.lexer.lineno += 1
 
+lexer = lex.lex()
 data = '''
-dynamic if 2var
+int goals;
+int goals = 5;
+double distance = 5.78;
+bool is_mvp = true;
+String = 'double qoute'
+var best_players = ['CR7', 'Kanté',     'Mbappe'];
+List best_players_2 = ['CR7', 'Kanté',   
+  'Mbappe'];
+List<int> numbers = [7, 10, 7];
+var position = {
+    'MCO': 'Noboa'
+    'MD': 'Plata'
+    'MI': 'Ibarra'
+}
+muster + 2;
+muster - 2;
+muster * 3;
+muster / 7;
+muster ~/ 7;
+muster += 6;
+muster -= 7;
+muster *= 7;
+muster /= 7;
+muster % 2;
+muster == 35;
+muster != 44;
+muster < 44;
+muster <= 33;
+44 > muster;
+muster >= 23;
+++muster;
+mustet++;
+--muster;
+muster--;
+%muster%;
+true && muster;
+true || muster;
+!true;
+siu('test', 43);
 '''
 
-# Give the lexer some input
-lexer.input(data)
+main = '''
+void main() { 
+    var num = 12; 
+    if (num % 2==0) { 
+        siu("Even"); 
+    } else { 
+        siu("Odd"); 
+    } 
+}
+'''
+for_data = '''
+for( var i = 0 ; i <= 10; i++ ) { 
+    siu(i); 
+}
+'''
+while_data = '''
+var i = 0;
+while(i <= 10) { 
+    siu(i);
+    i++; 
+} 
 
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok)
+'''
+if __name__ == "__main__":
+    lex.runmain(lexer, while_data)
