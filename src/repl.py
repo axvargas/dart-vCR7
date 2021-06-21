c479bf367829d80aed72e98fae45c38f6fca3a05
jcoboss <josuealexandercs15@gmail.com>
2021-06-20 22:03:18 -0500

    Remove unused files

--- src/repl.py
+++
@@ -1,9 +0,0 @@
-from src.lexer import Lexer
-
-
-def start_repl() -> None:
-    while (source := input('\n~#@❯ ')) != 'salir();':
-        lexer: Lexer = Lexer(source)
-
-        while (token := lexer.next_token()) != EOF_TOKEN:
-            print(token)
