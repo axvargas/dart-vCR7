2a09d92199fb8c2fe18f33ceab5c7394067f90d5
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-06-24 23:18:39 -0500

    my own file

--- src/andrew_yacc.py
+++ src/andrew_yacc.py
@@ -1,99 +1,60 @@
+'''
+Created Date: Thursday June 24th 2021 11:08:16 pm
+Author: Andrés X. Vargas
+-----
+Last Modified: Thursday June 24th 2021 11:11:04 pm
+Modified By: the developer known as Andrés X. Vargas at <axvargas@fiec.espol.edu.ec>
+-----
+Copyright (c) 2021 MattuApps
+'''
+
 from ply.yacc import yacc
 from lexer import tokens
 
 def p_body(p):
-  '''body : ifstm
-          | idmap
-		  | forstmt'''
+  '''body : forstmt'''
+
+def p_forstmt(p):
+    '''forstmt : FOR LPAREN INT_TYPE assign SEMICOLON comparisonint SEMICOLON varincredecre RPAREN LBRACE RBRACE
+    '''
 
 
-def p_ifstm(p):
-  '''ifstm : IF LPAREN condition RPAREN LBRACE RBRACE'''
+def p_assign(p):
+    '''assign : IDENT EQ_V INT
+    '''
 
-def p_condition(p):
-  '''condition : preposition operator preposition'''
 
-def p_condition_more(p):
-  '''condition :  preposition operator preposition morecond'''
+def p_comparisonint(p):
+    '''comparisonint : IDENT comparisonop INT
+    '''
 
-def p_morecond(p):
-  '''morecond : operator preposition
-              | operator preposition morecond
-  '''
 
-def p_operator(p):
-  '''operator : EQ
-              | NE
-              | AND
-              | OR
-  '''
+def p_varincredecre(p):
+    '''varincredecre : incredecre IDENT
+                                     | IDENT incredecre
+    '''
 
-def p_preposition(p):
-  '''preposition : typre'''
 
-def p_preposition_not(p):
-  '''preposition : NEGATION typre'''
+def p_incredecre(p):
+    '''incredecre : PLUSPLUS
+                              | MINUSMINUS
+    '''
 
-def p_typre(p):
-  '''typre : STRING
-           | INT
-           | TRUE
-           | FALSE
-           | FLOAT
-           | IDENT
-  '''
 
-def p_idmap(p):
-  '''idmap : tymap IDENT EQ_V dictionary'''
+def p_comparisonop(p):
+    '''comparisonop : EQ
+                        | GT
+                        | GTE
+                        | LT
+                        | LTE
+                        | NE
+    '''
 
-def p_tymap(p):
-  '''tymap : MAP
-           | MAP LT tykey COMMA tyvalue GT'''
-
-def p_tykey(p):
-  '''tykey : STRING_TYPE
-           | DOUBLE_TYPE
-           | INT_TYPE'''
-
-def p_tyvalue(p):
-  '''tyvalue : STRING_TYPE
-             | DOUBLE_TYPE
-             | INT_TYPE
-             | BOOLEAN
-             | IDENT
-             | iterable
-             | tymap'''
-
-def p_iterable(p):
-  '''iterable : LIST
-              | SET'''
-
-def p_dictionary(p):
-  '''dictionary : LBRACE item RBRACE'''
-
-def p_item(p):
-  '''item : key DDOTS value
-          | key DDOTS value COMMA item'''
-
-def p_key(p):
-  '''key : STRING
-         | INT
-         | FLOAT'''
-
-def p_value(p):
-  '''value : STRING
-           | FLOAT
-           | INT
-           | FALSE
-           | TRUE
-           | IDENT
-           | iterable
-           | tymap'''
 
 parser = yacc()
 
 print(
-'''
+    '''
 
     ██████╗░░█████╗░██████╗░████████╗  ░█████╗░██████╗░███████╗
     ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝  ██╔══██╗██╔══██╗╚════██║
@@ -115,11 +76,11 @@ Welcome to Dart CR7 v.0.1 REPL the programming language based on EL BICHO (SIIIU
 )
 
 while True:
-  try:
-    data = input('CR7>>> ')
-  except EOFError:
-    break
-  if not data: continue
-  result = parser.parse(data)
-  print(result)
-
+    try:
+        data = input('CR7>>> ')
+    except EOFError:
+        break
+    if not data:
+        continue
+    result = parser.parse(data)
+    print(result)
