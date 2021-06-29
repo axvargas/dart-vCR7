04929323444ba377cbae5e292c83a34392c0cc8f
enmanuel-mag <enmanuelmag@cardor.dev>
2021-06-28 23:17:13 -0500

    Add map test

--- src/testing/data.dart
+++ src/testing/data.dart
@@ -14,6 +14,13 @@ class MyApp {
     }
     List<int> lista = [1, 2, 3, 4, 5];
     lista.add(5);
+    Map<String, int> mapa = {
+      'uno': 1,
+      'dos': 2,
+      'tres': 3
+    };
+    mapa.remove('uno');
+    mapa.clear();
   }
 
 }
